8f42c8e25bc264c1d378732d798c0db2b5559e73
wbrandon25 <legobw1018@gmail.com>
2024-04-26 23:16:51 -0700

    Auto format with prettier

--- frontend/src/components/NewTask.jsx
+++ frontend/src/components/NewTask.jsx
@@ -1,46 +1,57 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function NewTask({ addTask }) {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const [isOpen, setIsOpen] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     addTask(title, content);
-    setTitle('');
-    setContent('');
-    setIsOpen(false); 
+    setTitle("");
+    setContent("");
+    setIsOpen(false);
   };
 
   const toggleForm = () => setIsOpen(!isOpen);
 
   return (
     <div>
-      <button className="bg-background-gray text-beak-orange rounded-lg py-2 px-4" onClick={toggleForm}>{isOpen ? 'Cancel' : 'Add Task'}</button>
+      <button
+        className="bg-background-gray text-beak-orange rounded-lg py-2 px-4"
+        onClick={toggleForm}
+      >
+        {isOpen ? "Cancel" : "Add Task"}
+      </button>
       {isOpen && (
-       <div className="mt-4 p-4 bg-white rounded-lg shadow-lg">
-       <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
-         <input
-           type="text"
-           value={title}
-           onChange={(e) => setTitle(e.target.value)}
-           placeholder="Name your task..."
-           required
-           className="p-2 border border-gray-300 focus:outline-none focus:border-beak-orange focus:ring-2 focus:ring-beak-orange rounded-lg"
-         />
-         <textarea
-           value={content}
-           onChange={(e) => setContent(e.target.value)}
-           placeholder="Describe your task..."
-           required
-           className="p-2 border border-gray-300 focus:outline-none focus:border-beak-orange focus:ring-2 focus:ring-beak-orange rounded-lg h-32"
-         />
-         <button className="self-end bg-background-gray text-beak-orange rounded-lg py-2 px-4" type="submit">
-           Submit Task
-         </button>
-       </form>
-     </div>
+        <div className="mt-4 p-4 bg-white rounded-lg shadow-lg">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col space-y-4"
+          >
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Name your task..."
+              required
+              className="p-2 border border-gray-300 focus:outline-none focus:border-beak-orange focus:ring-2 focus:ring-beak-orange rounded-lg"
+            />
+            <textarea
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              placeholder="Describe your task..."
+              required
+              className="p-2 border border-gray-300 focus:outline-none focus:border-beak-orange focus:ring-2 focus:ring-beak-orange rounded-lg h-32"
+            />
+            <button
+              className="self-end bg-background-gray text-beak-orange rounded-lg py-2 px-4"
+              type="submit"
+            >
+              Submit Task
+            </button>
+          </form>
+        </div>
       )}
     </div>
   );
