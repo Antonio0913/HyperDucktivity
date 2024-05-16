3ad68411f2d7feb6cf4260c716e950f30808198c
quinthemint <quinn.potter93@gmail.com>
2024-05-16 16:10:59 -0700

    settings routing and homepage design start

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ProfileIcon from "../assets/ProfileIcon.png"
+import SettingsIcon from "../assets/SettingsIcon.png"
+import LogoutIcon from "../assets/LogoutIcon.png"
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
+    <>
+    <div className="absolute top-0 left-0">
+      <div className="flex items-center space-x-4">
+      <h1 className="text-background-gray">HyperDucktivity</h1>
+      <div>
+        <p className="text-beak-yellow">
+          {" "}
+          _<br />
+          &#62;(.)__
+          <br />
+          &#40;___/
+          <br />
+          <br />
+        </p>
+      </div>
+      </div>
       <nav>
         <Link to="/categories">View Todo Categories</Link>
       </nav>
@@ -12,6 +29,16 @@ const Home = () => {
         <Link to="/taskPage">View TaskPage</Link>
       </nav>
     </div>
+    <div className="absolute bottom-0 left-0 flex items-center space-x-4">
+    <button>
+      <img src={ProfileIcon} alt="Profile Icon" className="w-31 h-17" />
+    </button>
+    <Link to="/settings">
+      <img src={SettingsIcon} alt="Settings Icon" className="w-31 h-17" />
+    </Link>
+      <img src={LogoutIcon} alt="Logout Icon" className="w-31 h-17" />
+    </div>
+    </>
   );
 };
 
---
+++ frontend/src/routes/Settings.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Settings = () => {
+  return (
+    <div>
+      <h1>(Placeholder Page) Todo Settings</h1>
+
+      <nav>
+        <Link to="/">Back to Home</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Settings;
