e6e24a5f9251d1e057cd93ab9a75202fac1a71c8
wbrandon25 <legobw1018@gmail.com>
2024-05-16 16:10:11 -0700

    Add clerk login, registration, logout

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  useUser
+} from "@clerk/clerk-react";
 
 const Home = () => {
+  const { user, isLoaded } = useUser();
+
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
+
       <nav>
         <Link to="/categories">View Todo Categories</Link>
       </nav>
       <nav>
         <Link to="/taskPage">View TaskPage</Link>
       </nav>
+      <br />
+
+      <header>
+        <SignedOut>
+          <SignInButton />
+          <p>User is not signed in</p>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+          <p>User is signed in</p>
+          {isLoaded && user && <p>Clerk User ID: {user.id}</p>}
+        </SignedIn>
+      </header>
     </div>
   );
 };
