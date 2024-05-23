48191cf3cada079029744db880bb062b19fad5ca
wbrandon25 <legobw1018@gmail.com>
2024-05-22 23:29:29 -0700

    Add category create, delete, page link

---
+++ frontend/src/components/CategoryItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+function CategoryItem({ category }) {
+  return (
+    <div className="flex justify-between items-center p-4 border border-gray-300 rounded-lg bg-gray-100 mb-4">
+      <Link
+        to={`/taskPage/${category._id}`}
+        className="font-bold text-lg text-blue-500 hover:underline"
+      >
+        {category.title}
+      </Link>
+    </div>
+  );
+}
+
+export default CategoryItem;
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -8,60 +8,77 @@ import {
   useUser,
   SignUpButton
 } from "@clerk/clerk-react";
-import ProfileIcon from "../assets/ProfileIcon.png"
-import SettingsIcon from "../assets/SettingsIcon.png"
-import LogoutIcon from "../assets/LogoutIcon.png"
+import ProfileIcon from "../assets/ProfileIcon.png";
+import SettingsIcon from "../assets/SettingsIcon.png";
+import LogoutIcon from "../assets/LogoutIcon.png";
+import NewCategory from "../components/NewCategory";
+import CategoryItem from "../components/CategoryItem";
 
 const Home = () => {
   const { user, isLoaded } = useUser();
 
   return (
     <>
-    <div className="absolute top-0 left-0">
-      <div className="flex items-center space-x-4">
-      <h1 className="text-background-gray">HyperDucktivity</h1>
-      <div>
-        <p className="text-beak-yellow">
-          {" "}
-          _<br />
-          &#62;(.)__
-          <br />
-          &#40;___/
-          <br />
-          <br />
-        </p>
-      </div>
-      </div>
-      <nav>
-        <Link to="/categories">View Todo Categories</Link>
-      </nav>
-      <nav>
-        <Link to="/taskPage">View TaskPage</Link>
-      </nav>
-      <br />
+      <div className="absolute top-0 left-0">
+        <div className="flex items-center space-x-4">
+          <h1 className="text-background-gray">
+            HyperDucktivity
+          </h1>
+          <div>
+            <p className="text-beak-yellow">
+              {" "}
+              _<br />
+              &#62;(.)__
+              <br />
+              &#40;___/
+              <br />
+              <br />
+            </p>
+          </div>
+        </div>
+        <nav>
+          <Link to="/taskPage">View TaskPage</Link>
+        </nav>
+        <br />
 
-      <header>
-        <SignedOut>
-          <SignInButton />
-          <SignUpButton style={{ marginLeft: "10px" }} />
-          <p>User is not signed in</p>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-          <p>User is signed in</p>
-          {isLoaded && user && <p>Clerk User ID: {user.id}</p>}
-        </SignedIn>
-      </header>
-    </div>
-    <div className="absolute bottom-0 left-0 flex items-center space-x-4">
-    <button>
-      <img src={ProfileIcon} alt="Profile Icon" className="w-31 h-17" />
-    </button>
-    <Link to="/settings">
-      <img src={SettingsIcon} alt="Settings Icon" className="w-31 h-17" />
-    </Link>
-      <img src={LogoutIcon} alt="Logout Icon" className="w-31 h-17" />
-    </div>
+        <br />
+        <header>
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton style={{ marginLeft: "10px" }} />
+            <p>User is not signed in</p>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+            <p>User is signed in</p>
+            {isLoaded && user && (
+              <p>Clerk User ID: {user.id}</p>
+            )}
+          </SignedIn>
+        </header>
+      </div>
+      <div className="absolute bottom-0 left-0 flex items-center space-x-4">
+        <button>
+          <img
+            src={ProfileIcon}
+            alt="Profile Icon"
+            className="w-31 h-17"
+          />
+        </button>
+        <Link to="/settings">
+          <img
+            src={SettingsIcon}
+            alt="Settings Icon"
+            className="w-31 h-17"
+          />
+        </Link>
+        <img
+          src={LogoutIcon}
+          alt="Logout Icon"
+          className="w-31 h-17"
+        />
+      </div>
+      <NewCategory />
     </>
   );
 };
