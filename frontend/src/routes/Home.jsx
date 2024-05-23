32607d60041f354ce08b7dc4e926995dee4de960
quinthemint <quinn.potter93@gmail.com>
2024-05-22 23:27:47 -0700

    homepage layout fixes

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
 
   return (
     <>
-    <div className="absolute top-0 left-0">
+    <div className="absolute top-2 left-2">
       <div className="flex items-center space-x-4">
       <h1 className="text-background-gray">HyperDucktivity</h1>
       <div>
@@ -39,28 +39,23 @@ const Home = () => {
         <Link to="/taskPage">View TaskPage</Link>
       </nav>
       <br />
-
-      <header>
-        <SignedOut>
+      <p>User is signed in</p>
+      {isLoaded && user && <p>Clerk User ID: {user.id}</p>}
+    </div>
+  
+    <div className="absolute bottom-3 left-3 flex items-center space-x-4">
+    <SignedOut>
           <SignInButton />
           <SignUpButton style={{ marginLeft: "10px" }} />
           <p>User is not signed in</p>
         </SignedOut>
         <SignedIn>
           <UserButton />
-          <p>User is signed in</p>
-          {isLoaded && user && <p>Clerk User ID: {user.id}</p>}
         </SignedIn>
-      </header>
-    </div>
-    <div className="absolute bottom-0 left-0 flex items-center space-x-4">
-    <button>
-      <img src={ProfileIcon} alt="Profile Icon" className="w-31 h-17" />
-    </button>
     <Link to="/settings">
       <img src={SettingsIcon} alt="Settings Icon" className="w-31 h-17" />
     </Link>
-      <img src={LogoutIcon} alt="Logout Icon" className="w-31 h-17" />
+      {/* <img src={LogoutIcon} alt="Logout Icon" className="w-31 h-17" /> */}
     </div>
     </>
   );
