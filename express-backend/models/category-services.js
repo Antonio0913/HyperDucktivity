49a8683226e53a20cad6a9e2c429e9c5e4ec4edc
wbrandon25 <legobw1018@gmail.com>
2024-05-22 19:40:22 -0700

    Create category-services.js

---
+++ express-backend/models/category-services.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import categoryModel from "./category.js";
+
+mongoose.set("debug", true);
+
+mongoose
+  .connect("mongodb://localhost:27017/tasks", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .catch((error) => console.log(error));
+
+function getCategories(title) {
+  let promise;
+  promise = categoryModel.find().populate("taskList");
+  //   if (title === undefined) {
+  //     promise = categoryModel.find().populate("taskList");
+  //   } else if (title) {
+  //     promise = findCategoryByName(title);
+  //   }
+  return promise;
+}
+
+function findCategoryById(id) {
+  return categoryModel.findById(id).populate("taskList");
+}
+
+function addCategory(category) {
+  const categoryToAdd = new categoryModel(category);
+  const promise = categoryToAdd.save();
+  return promise;
+}
+
+function removeCategory(id) {
+  return categoryModel.findByIdAndDelete(id);
+}
+
+function addTaskToCategory(categoryId, taskId) {
+  return categoryModel
+    .findByIdAndUpdate(
+      categoryId,
+      { $push: { taskList: taskId } },
+      { new: true, useFindAndModify: false }
+    )
+    .populate("taskList");
+}
+
+export default {
+  addCategory,
+  getCategories,
+  findCategoryById,
+  //   findCategoryByName,
+  removeCategory,
+  addTaskToCategory
+};
