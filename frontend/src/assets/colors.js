8cb4c79f244a38643c96a88fe56052b8ec93bca6
wbrandon25 <legobw1018@gmail.com>
2024-05-30 20:04:14 -0700

    refactor: prettier autoformat the entire project

--- frontend/src/assets/colors.js
+++ frontend/src/assets/colors.js
@@ -1,9 +1,9 @@
 const colors = {
-    "beak-yellow": "#ECD632",
-    "beak-orange": "#bb4910",
-    "main-brown": "#a66e46",
-    "background-gray": "#BEB1A4",
-    "site-bg" : "#242424"
-  }
+  "beak-yellow": "#ECD632",
+  "beak-orange": "#bb4910",
+  "main-brown": "#a66e46",
+  "background-gray": "#BEB1A4",
+  "site-bg": "#242424"
+};
 
 export default colors;
