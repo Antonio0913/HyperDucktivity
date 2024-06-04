5ff8616db0a8ff2fb81f0103b30734ccd6596217
wbrandon25 <legobw1018@gmail.com>
2024-06-04 15:25:43 -0700

    revert vite config, add routes.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: "src/main.jsx"
-    }
-  }
+  plugins: [react()]
 });
