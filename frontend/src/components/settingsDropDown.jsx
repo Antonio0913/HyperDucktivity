8bd82f97fff66987147b02ebb6ca4b1c2514cdd8
Justin-Koida-SA <100797123+Justin-Koida-SA@users.noreply.github.com>
2024-06-04 22:42:55 -0700

    duck

--- frontend/src/components/settingsDropDown.jsx
+++ frontend/src/components/settingsDropDown.jsx
@@ -25,15 +25,24 @@ const SettingsDropDown = ({ username, logout, textSize, setTextSize }) => {
             <strong>User: {username}</strong>
           </div>
           <div className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer flex items-center space-x-2">
-            <img src={TextSizeIcon} alt="Text Size Icon" className="w-6 h-6" />
-            <span>Text Size</span>
+            <img src={TextSizeIcon} alt="Text Size Icon" className="w-12 h-12" />
+            <div className="block px-4 py-2">
+              <FontSize textSize={textSize} setTextSize={setTextSize} />
+            </div>
           </div>
-          <div className="block px-4 py-2">
-            <FontSize textSize={textSize} setTextSize={setTextSize} />
-          </div>
-          <div className="block px-4 py-2 text-red-600 hover:bg-gray-100 cursor-pointer flex items-center space-x-2" onClick={logout}>
-            <img src={LogoutIcon} alt="Logout Icon" className="w-6 h-6" />
-            <span>Logout</span>
+          <div
+            className="block px-4 py-2 text-red-600 hover:bg-gray-100 cursor-pointer flex items-center space-x-2 justify-between"
+            onClick={logout}
+          >
+            <img src={LogoutIcon} alt="Logout Icon" className="w-12 h-12" />
+            <p className="text-beak-yellow text-right">
+              <br />
+              &#62;(.)__
+              <br />
+              &#40;___/
+              <br />
+              <br />
+            </p>
           </div>
         </div>
       )}
